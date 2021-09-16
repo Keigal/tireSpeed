b68839e2c7dfb3ed4bf9d1b9a327ac4436a31b8c
Keigal <34088122+Keigal@users.noreply.github.com>
2021-09-16 14:39:07 -0500

    init commit

---
+++ client/main.lua
@@ -0,0 +1,52 @@
+
+-- Thread checks tire status and acts appropriately
+Citizen.CreateThread(function()
+
+    while true do
+
+        -- Due to the nature of how wheels pop this thread doesn't need to run often
+        Citizen.Wait(1000)
+
+        local playerPed = PlayerPedId()
+
+        local vehicle = GetVehiclePedIsIn(playerPed)
+
+        local maxTires = GetVehicleNumberOfWheels(vehicle)
+
+        local poppedTires = 0
+
+        -- Gets number of poppedTires
+        for i = 0, maxTires, 1
+        do
+            local wheelHealth = GetVehicleWheelHealth(vehicle, i)
+            
+            if (wheelHealth == 0)
+            then
+                poppedTires = poppedTires + 1
+            end
+
+        end
+
+        -- Logic for limiting speed
+
+        TriggerEvent('chat:addMessage', {
+            color = {255, 0, 0},
+            multiline = true,
+            args = {'Max tires: ', maxTires}
+        })
+
+        TriggerEvent('chat:addMessage', {
+            color = {255, 0, 0},
+            multiline = true,
+            args = {'Popped tires: ', poppedTires}
+        })
+
+        TriggerEvent('chat:addMessage', {
+            color = {255, 0, 0},
+            multiline = false,
+            args = {'tireSpeed ran'}
+        })
+
+    end
+
+end)
