10158ff5ca5051a282ef106678ffd1bf125aad54
Keigal <34088122+Keigal@users.noreply.github.com>
2021-09-16 15:11:32 -0500

    ts

--- client/main.lua
+++ client/main.lua
@@ -5,54 +5,53 @@ Citizen.CreateThread(function()
     while true do
 
         -- Due to the nature of how wheels pop this thread doesn't need to run often
-        Citizen.Wait(1000)
+        Citizen.Wait(3000)
 
         local playerPed = PlayerPedId()
 
         local vehicle = GetVehiclePedIsIn(playerPed, false)
 
-        TriggerEvent('chat:addMessage', {
-            color = {255, 0, 0},
-            multiline = true,
-            args = {'Vehicle', vehicle}
-        })
+        if !(vehicle == 0)
+        then
 
-        local maxTires = GetVehicleNumberOfWheels(vehicle)
+            local maxTires = GetVehicleNumberOfWheels(vehicle)
 
-        local poppedTires = 0
+            local poppedTires = 0
 
-        -- Gets number of poppedTires
-        for i = 0, maxTires, 1
-        do
-            local wheelHealth = GetVehicleWheelHealth(vehicle, i)
+            -- Gets number of poppedTires
+            for i = 0, maxTires, 1
+            do
+                -- local wheelHealth = GetVehicleWheelHealth(vehicle, i)
+
+                -- TriggerEvent('chat:addMessage', {
+                --     color = {255, 0, 0},
+                --     multiline = true,
+                --     args = {'Tire Health', wheelHealth}
+                -- })
+                
+                if (IsVehicleTyreBurst(vehicle, i, false))
+                then
+                    poppedTires = poppedTires + 1
+                end
+
+            end
+
+            -- Logic for limiting speed
+
+            -- TriggerEvent('chat:addMessage', {
+            --     color = {255, 0, 0},
+            --     multiline = true,
+            --     args = {'Max tires: ', maxTires}
+            -- })
 
             TriggerEvent('chat:addMessage', {
                 color = {255, 0, 0},
                 multiline = true,
-                args = {'Tire Health', wheelHealth}
+                args = {'Popped tires: ', poppedTires}
             })
-            
-            if (wheelHealth == 0)
-            then
-                poppedTires = poppedTires + 1
-            end
 
         end
-
-        -- Logic for limiting speed
-
-        -- TriggerEvent('chat:addMessage', {
-        --     color = {255, 0, 0},
-        --     multiline = true,
-        --     args = {'Max tires: ', maxTires}
-        -- })
-
-        -- TriggerEvent('chat:addMessage', {
-        --     color = {255, 0, 0},
-        --     multiline = true,
-        --     args = {'Popped tires: ', poppedTires}
-        -- })
-
+    
     end
 
 end)
