c38d6f530e72771247434427cfb14637f58494eb
Keigal <34088122+Keigal@users.noreply.github.com>
2021-09-16 23:46:59 -0500

    readded debug statments

--- server/main.lua
+++ server/main.lua
@@ -5,11 +5,28 @@ Citizen.CreateThread(function()
 
         Citizen.Wait(10)
 
+        TriggerClientEvent('chat:addMessage', -1, {
+            color = {255, 0, 0},
+            multiline = true,
+            args = {'Server', 'Inside while loop'}
+        })
+
         -- Get all players in server
         for _, playerId in ipairs(GetPlayers()) do
 
             local ped = GetPlayerPed(playerId)
             local veh = GetVehiclePedIsIn(ped, false)
+
+            TriggerClientEvent('chat:addMessage', -1, {
+                color = {255, 0, 0},
+                multiline = true,
+                args = {'server-ped', ped}
+            })
+            TriggerClientEvent('chat:addMessage', -1, {
+                color = {255, 0, 0},
+                multiline = true,
+                args = {'server-veh', veh}
+            })
 
             -- Checks if ped is in vehicle
             if (veh ~= 0)
@@ -18,8 +35,20 @@ Citizen.CreateThread(function()
                 -- Gets network id of vehicle
                 local vehNetID = NetworkGetNetworkIdFromEntity(veh)
 
+                TriggerClientEvent('chat:addMessage', -1, {
+                    color = {255, 0, 0},
+                    multiline = true,
+                    args = {'server-vehNetID: ', vehNetID}
+                })
+
                 -- Gets owner of vehicle entity
                 local owner = NetworkGetEntityOwner(vehNetID)
+
+                TriggerClientEvent('chat:addMessage', -1, {
+                    color = {255, 0, 0},
+                    multiline = true,
+                    args = {'server-owner: ', vehNetID}
+                })
 
                 -- Tells owner's client to run event to limit speed
                 TriggerClientEvent('tireSpeed:client:checkTires', owner, owner)
