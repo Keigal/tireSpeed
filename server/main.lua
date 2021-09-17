91f21e5291896f311f8cf62520a0a48eff7fa5fc
Keigal <34088122+Keigal@users.noreply.github.com>
2021-09-16 22:36:21 -0500

    debug

--- server/main.lua
+++ server/main.lua
@@ -3,6 +3,12 @@ Citizen.CreateThread(function()
 
     while true do
 
+        TriggerClientEvent('chat:addMessage', -1, {
+            color = {255, 0, 0},
+            multiline = true,
+            args = {'Thread ran.'}
+        })
+
         Citizen.Wait(5000)
 
         -- Get all players in server
@@ -10,6 +16,17 @@ Citizen.CreateThread(function()
 
             local ped = GetPlayerPed(playerId)
             local veh = GetVehiclePedIsIn(ped, false)
+
+            TriggerClientEvent('chat:addMessage', -1, {
+                color = {255, 0, 0},
+                multiline = true,
+                args = {'server-ped', ped}
+            })
+            TriggerClientEvent('chat:addMessage', -1, {
+                color = {255, 0, 0},
+                multiline = true,
+                args = {'server-veh', veh}
+            })
 
             -- Checks if ped is in vehicle
             if (veh ~= 0)
@@ -37,12 +54,6 @@ Citizen.CreateThread(function()
                 TriggerClientEvent('tireSpeed:client:checkTires', owner, owner)
 
             end
-
-            TriggerClientEvent('chat:addMessage', -1, {
-                color = {255, 0, 0},
-                multiline = true,
-                args = {'Thread ran.'}
-            })
 
         end
 
